3e90cbba5081d95563ef4b91ca038908fd47dcf4
sunqi <sunqi@xiao100.com>
2021-10-22 17:34:00 +0800

    feat: css modules & scss

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import styles from 'styles/first-post.module.scss'
 
 // console.log('执行了')
 export default function FirstPost() {
@@ -9,9 +10,10 @@ export default function FirstPost() {
   }, [])
   return (
     <>
-      <div>First Post <button onClick={clickMe}>click me</button>
-        <hr/>
-        回到首页 <Link href='/'><a>点击这里</a></Link>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          内容
+        </div>
       </div>
     </ >
   )

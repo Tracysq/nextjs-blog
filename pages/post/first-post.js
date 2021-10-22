88b0cbb3617533848258200f0bd129d74df29e50
sunqi <sunqi@xiao100.com>
2021-10-22 16:34:12 +0800

    feat: 局部修改 Head

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,15 +1,22 @@
 import React, { useCallback } from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
-console.log('执行了')
+// console.log('执行了')
 export default function X() {
   const clickMe = useCallback(() => {
     console.log('you clicked me')
   }, [])
   return (
-    <div>First Post <button onClick={clickMe}>click me</button>
-      <hr/>
-      回到首页 <Link href='/'><a>点击这里</a></Link>
-    </div>
+    <>
+      <Head>
+        <title> 第一篇文章</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"></meta>
+      </Head>
+      <div>First Post <button onClick={clickMe}>click me</button>
+        <hr/>
+        回到首页 <Link href='/'><a>点击这里</a></Link>
+      </div>
+    </ >
   )
-}
+} 
